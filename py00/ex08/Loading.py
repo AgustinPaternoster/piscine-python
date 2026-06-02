8b524160b456705df3c70fdbdcba27f07ab0ae35
AgustinPaternoster <agustinpaternoster@gmail.com>
2026-06-02 19:42:10 +0200

    py00 ex08 done

--- py00/ex08/Loading.py
+++ py00/ex08/Loading.py
@@ -5,18 +5,21 @@ import os
 def ft_tqdm(lst: range)-> None:
     
     terminalSize = os.get_terminal_size().columns
-    arr = [ n + 1 for n in lst]
-    total = arr[-1]
+    total = len(lst)
     counterWidth = int(len(str(total)))
     completed = 0
     barSize = terminalSize - ((counterWidth * 2) + 2) - 4 - 4 - 25
-    for i in arr:
-        percentage = int((i / total) * 100)
+    for x in lst:
         completed  += 1
-        barCompletion = int((barSize/total) * i)
+        percentage = int((completed / total) * 100)
+        barCompletion = int((barSize/total) * completed)
         barEmpty = barSize - barCompletion
+        if completed == total:
+            barString = "=" * barCompletion
+        elif completed > 0:
+            barString = ("=" * (barCompletion - 1)) + ">"
         print(f'{percentage:3}%', end='') # porcentaje
-        print(f'|[{"=" * barCompletion}{" " * barEmpty}]|', end=" ") # barra
+        print(f'|[{barString}{" " * barEmpty}]|', end=" ") # barra
         print(f'{completed:{counterWidth}}/{total}', end='')
         print('\r', end='') # final
         yield
