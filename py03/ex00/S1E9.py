5c3a9ac908f0fddc1fd0610d3ae8e2c3d1a9496f
AgustinPaternoster <agustinpaternoster@gmail.com>
2026-07-01 18:54:35 +0200

    WIP py03 ex00

---
+++ py03/ex00/S1E9.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    def __init__(self, first_name, is_alive=True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+    
+    @abstractmethod
+    def die(self):
+        pass
+
+class Stark(Character):
+    def is_alive(self,is_alive: bool):
+        self.is_alive = is_alive
