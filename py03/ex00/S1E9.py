450cf432f7ae060f31616ca7b515077f7da62b06
AgustinPaternoster <agustinpaternoster@gmail.com>
2026-07-02 14:15:11 +0200

    py03 ex00 done

--- py03/ex00/S1E9.py
+++ py03/ex00/S1E9.py
@@ -1,14 +1,23 @@
 from abc import ABC, abstractmethod
 
+
 class Character(ABC):
+    """Abstract class representing a character in a story."""
+
     def __init__(self, first_name, is_alive=True):
+        """Initialize a Character with first name and alive status."""
         self.first_name = first_name
         self.is_alive = is_alive
-    
+
     @abstractmethod
     def die(self):
+        """Change the alive status of the character to False."""
         pass
 
+
 class Stark(Character):
-    def is_alive(self,is_alive: bool):
-        self.is_alive = is_alive
+    """Concrete class representing a Stark character."""
+
+    def die(self):
+        """Change the alive status of the Stark to False."""
+        self.is_alive = False
